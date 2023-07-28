c1a4ca36c22ceb1b11dc51ab7e6391670278e463
Omar R <omar.ahmedradwan@vodafone.com>
2023-07-28 19:53:05 +0300

    Initial Commit

---
+++ BasicsOfCombine/BasicsOfCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicsOfCombineApp.swift
+//  BasicsOfCombine
+//
+//  Created by Omar Radwan on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicsOfCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
